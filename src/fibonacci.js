8bca345b320819c4bdea635ff2a907b037f588ef
leikkisa <sallymaki@gmail.com>
2017-05-22 16:37:52 -0700

    Finish fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,9 +1,18 @@
-export default function fibonacci({price, amountGiven}) {
-  // your code here
-}
-#### fibonacci
+// Returns an array of Fibonacci numbers to the nth position.
 
-Return an array of Fibonacci numbers to the _nth_ position.
+export default function fibonacci(n) {
+  let current = 0
+  let result = []
+  while (current < n) {
+    if (current < 2) {
+      result.push(current)
+    } else {
+      result.push(result[current-1]+result[current-2])
+      }
+    current++
+  }
+  return result
+}
 
 ```javascript
 fibonacci(10)
