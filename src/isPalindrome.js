dda5efe9918c36de7b4cf67f1b62f00846089db7
leikkisa <sallymaki@gmail.com>
2017-05-22 15:03:49 -0700

    Finish isPalindrome

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -1,20 +1,14 @@
-export default function isPalindrome({price, amountGiven}) {
-  // your code here
+// Determines if a string is a palindrome. Returns `true` or `false`.
+// Ignores punctuation, spacing, and case sensitivity.
+
+export default function   cleanString = string.toLowerCase().replace(/[^a-z]/g, '')
+  reversedString = Array.prototype.map.call(cleanString, function(x) {
+  return x
+  }).reverse().join('');
+  if (cleanString === reversedString) {
+    return true
+  } else {
+    return false
+
+  }
 }
-
-#### isPalindrome
-
-Determine if a string is a palindrome. Return `true` or `false`.
-
-Ignore punctuation, spacing, and case sensitivity.
-
-```javascript
-isPalindrome('radar')
-// => true
-
-isPalindrome('bananas')
-// => false
-
-isPalindrome('A man, a plan, a canal: Panama')
-// => true
-```
--- test/isPalindrome_test.js
+++ test/isPalindrome_test.js
@@ -7,47 +7,16 @@ describe('isPalindrome()', function(){
     expect(isPalindrome).to.be.a('function')
   })
 
-  it('returns an object with all coin types (quarters, dimes, nickels, and pennies)', function(){
-    const change = isPalindrome({price: 100, amountGiven: 100})
-    expect(change).to.be.an('object')
-    expect(change).to.have.keys('quarters', 'dimes', 'nickels', 'pennies')
-    expect(change).to.deep.equal({
-      quarters: 0,
-      dimes: 0,
-      nickels: 0,
-      pennies: 0,
-    })
+  it('Return true for palindrome)',
+    expect(isPalindrome('radar')).to.equal(true)
   })
 
-  it('returns correct change', function(){
-    expect(isPalindrome({price: 100, amountGiven: 141})).to.deep.equal({
-      quarters: 1,
-      dimes: 1,
-      nickels: 1,
-      pennies: 1,
-    })
+  it('Return false for not palindrome)',
+    expect(isPalindrome('bananas')).to.equal(false)
   })
 
-  it('minimizes the number of coins given by using the most high-value coins', function(){
-    expect(isPalindrome({price: 100, amountGiven: 168})).to.deep.equal({
-      quarters: 2,
-      dimes: 1,
-      nickels: 1,
-      pennies: 3,
-    })
-
-    expect(isPalindrome({price: 100, amountGiven: 169})).to.deep.equal({
-      quarters: 2,
-      dimes: 1,
-      nickels: 1,
-      pennies: 4,
-    })
-
-    expect(isPalindrome({price: 100, amountGiven: 170})).to.deep.equal({
-      quarters: 2,
-      dimes: 2,
-      nickels: 0,
-      pennies: 0,
-    })
+  it('Return true for palindrome with spaces and punctuation)',
+    expect(isPalindrome('A man, a plan, a canal: Panama')).to.equal(true)
   })
+
 })
